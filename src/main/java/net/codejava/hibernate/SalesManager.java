58836fd21e6a7102bf26ff209a208a5e120774e4
Rishabh3095 <rishabh.gupta@sjsu.edu>
2018-05-05 00:55:31 -0700

    Working on retrieving the data from database

--- src/main/java/net/codejava/hibernate/SalesManager.java
+++ src/main/java/net/codejava/hibernate/SalesManager.java
@@ -1,5 +1,11 @@
 package net.codejava.hibernate;
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Date;
+import java.util.Scanner;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -11,7 +17,7 @@ public class SalesManager {
 	
 	//
     protected SessionFactory sessionFactory;
-    
+
     protected void setup() {
         // code to load Hibernate Session factory
     	final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -33,26 +39,96 @@ public class SalesManager {
         // code to save
     	SalesTransaction satr = new SalesTransaction();
        
-        Date date = new Date();  
-        
+    	boolean inputCheck = false;
+    	Scanner numb = new Scanner(System.in);
+    	Scanner str = new Scanner(System.in);
+    	
+    	System.out.println("Enter the name of the product: ");
+    	
+    	String prodName = "";
+    	while(!inputCheck)
+    	{
+        	prodName = str.nextLine();
+        	if(!prodName.trim().equals(""))
+        		System.out.println("Please enter valid input: ");
+        	inputCheck = true;
+    	}
+    	
+    	System.out.println("Enter the quantity: ");
+    	
+    	int quantity = numb.nextInt();    			
+    	
+    	System.out.println("Enter the total cost of the bulk: ");
+    	int totalCost = numb.nextInt();
+    	System.out.println("Enter the cost per piece: ");
+    	int unitCost = numb.nextInt();
+    	
+    	Date date = new Date();
     	satr.setDate(date);
-    	satr.setProductName("Tie");
-    	satr.setQuantity(20);
-    	satr.setTotalCost(120);
-    	satr.setUnitCost(6);
+    	satr.setProductName(prodName);
+    	satr.setQuantity(quantity);
+    	satr.setTotalCost(totalCost);
+    	satr.setUnitCost(unitCost);
     	
         Session session = sessionFactory.openSession();
         session.beginTransaction();
      
         session.save(satr);
         
-     
+        numb.close();
+        str.close();
         session.getTransaction().commit();
         session.close();
     }
  
-    protected void read() {
-        // code to get a book
+    protected void read() throws ParseException {
+        // code to get
+    	
+    	Scanner in = new Scanner(System.in);
+    	
+    	System.out.println("Enter the details for the first date: ");
+    	System.out.println("Enter the year: ");
+    	int year = in.nextInt();
+    	System.out.println("Enter the month: ");
+    	int month = in.nextInt();
+    	System.out.println("Enter the date: ");
+    	int d = in.nextInt();
+    	Month m = Month.of(month);
+
+    	LocalDate localid = LocalDate.of(year, m, d);
+        java.sql.Date date1 = java.sql.Date.valueOf(localid);
+
+    	System.out.println("Enter the details for the second date: ");
+    	System.out.println("Enter the year: ");
+    	int year2 = in.nextInt();
+    	System.out.println("Enter the month: ");
+    	int month2 = in.nextInt();
+    	System.out.println("Enter the date: ");
+    	int d2 = in.nextInt();
+    	Month m2 = Month.of(month2);
+
+    	LocalDate localid2 = LocalDate.of(year2, m2, d2);
+        java.sql.Date date2 = java.sql.Date.valueOf(localid2);
+        
+    	Session session = sessionFactory.openSession();
+        session.beginTransaction();
+    	
+        
+        //get all the dates from the date1 to date2
+        
+    	SalesTransaction input = session.get(SalesTransaction.class, date2);
+    	
+    	System.out.println("Date: "+input.getDate());
+    	System.out.println("Product Name: "+input.getProductName());
+    	System.out.println("Total Quantity: "+input.getQuantity());
+    	System.out.println("Bulk Cost: "+input.getTotalCost());
+    	System.out.println("Unit Cost: "+input.getUnitCost());
+    	
+
+    	in.close();
+        session.getTransaction().commit();
+        session.close();
+        
     }
  
     protected void update() {
@@ -63,11 +139,12 @@ public class SalesManager {
         // code to remove a book
     }
  
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // code to run the program
         SalesManager manager = new SalesManager();
         manager.setup();
         manager.create();
+        manager.read();
         manager.exit();
     }
 }
